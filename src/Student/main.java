f8878d61ce9d8921de50e29605f0700af379b2f2
Emin Novruzov <inovruzovemin@gmail.com>
2024-11-21 19:07:37 +0400

    Update main.java

--- src/Student/main.java
+++ src/Student/main.java
@@ -1,4 +1,15 @@
 package PACKAGE_NAME.Student;
 
 public class main {
+    public static void main(String[] args) {
+        // student üçün
+        Student student = new Student("Elmler","BDU", 1,"English", "Ali");
+        student.study();
+
+        Teacher teacher = new Teacher("28 May","ADNSU",45,"Mats", "Su");
+        teacher.teach();
+
+        Academy academy =  new Academy("City Point", "ATL");
+        academy.display();
+    }
 }
