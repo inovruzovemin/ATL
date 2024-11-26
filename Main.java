7f98ee69fb2e6fea9714fbbedf8c77d33dd1b720
Emin Novruzov <inovruzovemin@gmail.com>
2024-11-26 17:26:33 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package PACKAGE_NAME.Interface;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.startCar();
+
+//        Bike bike = new Bike();
+//        bike.startEngine();
+//        bike.stopEngine();
+//        System.out.println(bike.getFuelType());
+//
+//            ElectricCar electricCar = new ElectricCar();
+//            electricCar.startEngine();
+//            electricCar.stopEngine();
+//            System.out.println(electricCar.getFuelType());
+
+    }
+}
