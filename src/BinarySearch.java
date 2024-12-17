e8e1d65994e5900dab33b28e34fd6f214e22fb13
inovruzovemin <inovruzovemin@gmail.com>
2024-12-17 13:19:45 +0400

    d

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -7,20 +7,21 @@ public class BinarySearch {
         int last = number.length-1;
         int middle = (first+last)/2;
         int search =75;
-        int count = 0;
+        int count = 0; // 25,
 
-            for(int num=0;num<last;num++){
-                if(number[middle]<search){
-                    num=last+1;
-                    count++;
-                    System.out.println(count);
-            }
-                else if(number[middle]>search){
-                    last=middle;
+            for(int f=0;f<last;f++){
+                if(number[middle]==search) {
+                    System.out.println("Search number found. Index N-" + middle );
+                }
+                else if(number[middle]<search) {
+                    first = middle+1;
+                    middle = (last - first)/2;
                     count++;
                 }
+
                 else {
-                    System.out.println("This number don't available");
+                    last= middle -1;
+                    middle=(last-first)/2;
                 }
         }
         System.out.println(count);
