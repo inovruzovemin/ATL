2a22928a7fb03d29a8531e0c3df9f1cc8060a9bd
inovruzovemin <inovruzovemin@gmail.com>
2024-12-17 14:02:55 +0400

    s

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -2,31 +2,34 @@ package PACKAGE_NAME;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int [] number = {1,2,5,10,25,35,50,75,100};
-        int first =0;
-        int last = number.length-1;
-        int middle = (first+last)/2;
-        int search =75;
+        int[] number = {1, 2, 5, 10, 25, 35, 50, 75, 100};
+        int first = 0;
+        int last = number.length - 1;
+        int middle = (first + last) / 2;
+        int search = 75;
         int count = 0; // 25,
+        boolean avi= false;
 
-            for(int f=0;f<last;f++){
-                if(number[middle]==search) {
-                    System.out.println("Search number found. Index N-" + middle );
+
+            for (int f = 0; f < last; f++) {
+                if (number[middle] == search) {
+                    System.out.println("Search number found. Index N-" + middle);
+                    avi= true;
                     break;
-                }
-                else if(number[middle]<search) {
-                    first = middle+1;
-                    middle = (last + first)/2;
+                } else if (number[middle] < search) {
+                    first = middle + 1;
+                    middle = (last + first) / 2;
+                    count++;
+                } else {
+                    last = middle - 1;
+                    middle = (last + first) / 2;
                     count++;
                 }
 
-                else {
-                    last= middle -1;
-                    middle=(last+first)/2;
-                }
+            }
+            System.out.print(avi ? "Count : " + count : "");
+        System.out.println(!avi ? "Number not founded" : "");
         }
-        System.out.println(count);
 
     }
 
-}
